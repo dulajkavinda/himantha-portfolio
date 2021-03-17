e10bd702f1f64e2094b1e7878e1e018c3ce781a2
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-03-18 00:15:46 +0530

    center web header

--- src/components/Header.js/Header.js
+++ src/components/Header.js/Header.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import Image from './Image';
-import Text from './Text'
-import Button from './Button'
+import React from "react";
+import Image from "./Image";
+import Text from "./Text";
+import Button from "./Button";
 
 export default function Header() {
-    return (
-        <div>
-        <div class="container">
-        <div class="row" style={{marginTop: "2rem"}}>
-            <div class="col-3">
-            <Image/>
-            </div>
-            <div class="col-7">
-            <Text/>
-            </div>
-            <div class="col-2">
-            <Button/>
-            </div>
+  return (
+    <div>
+      <div class="container">
+        <div
+          style={{
+            marginTop: "2rem",
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+          }}
+        >
+          <div style={{ textAlign: "center" }}>
+            <Image />
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <Text />
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <Button />
+          </div>
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/components/Header.js/Text.js
+++ src/components/Header.js/Text.js
@@ -1,24 +1,64 @@
-import React from 'react'
+import React from "react";
 
 export default function Text() {
-    return (
-        <div>
-        <div className="row"><h1 style={{fontWeight:"bold"}}>Himantha Tharindu</h1></div>
-        <div className="row"><h3 style={{color: "gray"}}>Software Developer</h3></div>
-            <div class="row">
-                <a href="https://pasindu.me/"class="p-2 rounded-circle" style={{background:"grey", marginRight: "0.5rem"}}>
-                    <img src={process.env.PUBLIC_URL +'./icons8-facebook-96 .png'} class="rounded-circle" alt="himantha" width="30rem"/>
-                </a>
-                <a href="https://pasindu.me/"class="p-2 rounded-circle" style={{background:"grey", marginRight: "0.5rem"}}>
-                    <img src={process.env.PUBLIC_URL +'./icons8-linkedin-96.png'} class="rounded-circle" alt="himantha" width="30rem"/>
-                </a>
-                <a href="https://pasindu.me/"class="p-2 rounded-circle" style={{background:"grey", marginRight: "0.5rem"}}>
-                    <img src={process.env.PUBLIC_URL +'./icons8-instagram-96 .png'} class="rounded-circle" alt="himantha" width="30rem"/>
-                </a>
-                <a href="https://pasindu.me/"class="p-2 rounded-circle" style={{background:"grey", marginRight: "0.5rem"}}>
-                    <img src={process.env.PUBLIC_URL +'./icons8-email-96 .png'} class="rounded-circle" alt="himantha" width="30rem"/>
-                </a>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="row">
+        <h1 style={{ fontWeight: "bold" }}>Himantha Tharindu</h1>
+      </div>
+      <div>
+        <h3 style={{ color: "gray" }}>Software Developer</h3>
+      </div>
+      <div class="row" style={{ display: "flex", justifyContent: "center" }}>
+        <a
+          href="https://pasindu.me/"
+          class="p-2 rounded-circle"
+          style={{ background: "grey", marginRight: "0.5rem" }}
+        >
+          <img
+            src={process.env.PUBLIC_URL + "./icons8-facebook-96 .png"}
+            class="rounded-circle"
+            alt="himantha"
+            width="30rem"
+          />
+        </a>
+        <a
+          href="https://pasindu.me/"
+          class="p-2 rounded-circle"
+          style={{ background: "grey", marginRight: "0.5rem" }}
+        >
+          <img
+            src={process.env.PUBLIC_URL + "./icons8-linkedin-96.png"}
+            class="rounded-circle"
+            alt="himantha"
+            width="30rem"
+          />
+        </a>
+        <a
+          href="https://pasindu.me/"
+          class="p-2 rounded-circle"
+          style={{ background: "grey", marginRight: "0.5rem" }}
+        >
+          <img
+            src={process.env.PUBLIC_URL + "./icons8-instagram-96 .png"}
+            class="rounded-circle"
+            alt="himantha"
+            width="30rem"
+          />
+        </a>
+        <a
+          href="https://pasindu.me/"
+          class="p-2 rounded-circle"
+          style={{ background: "grey", marginRight: "0.5rem" }}
+        >
+          <img
+            src={process.env.PUBLIC_URL + "./icons8-email-96 .png"}
+            class="rounded-circle"
+            alt="himantha"
+            width="30rem"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
